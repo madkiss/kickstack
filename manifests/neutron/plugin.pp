c38eb0bad48e984b6489bd592ec3128cc8a17934
Martin Loschwitz <martin.loschwitz@hastexo.com>
2014-04-22 14:06:17 +0200

    Add ML2 parameters to the kickstack::neutron::plugin class
    This commit enables the kickstack::neutron::plugin class to support the
    ML2 plugin in OpenStack Neutron. It ensures that network_vlan_ranges and
    tunnel_id_ranges are set properly depending on the plug-in that is used.
    For OVS or Linuxbridge setups, these need to be strings. In ML2 setups,
    they need to be arrays.
    
    It also deletes a workaround for a previous bug in the Neutron plugin
    for Open vSwitch that avoided proper functioning of the firewall_driver
    variable. The bug in the Neutron plugin has been fixed by now, and thus
    the workaround is not necessary anymore.

--- manifests/neutron/plugin.pp
+++ manifests/neutron/plugin.pp
@@ -3,14 +3,45 @@ class kickstack::neutron::plugin inherits kickstack {
   include kickstack::neutron::config
 
   $sql_conn = getvar("${fact_prefix}neutron_sql_connection")
-  $tenant_network_type = "$::kickstack::neutron_tenant_network_type"
-  $network_vlan_ranges = $tenant_network_type ? {
-    'gre' => '',
-    default => "${::kickstack::neutron_physnet}:${::kickstack::neutron_network_vlan_ranges}",
+  $ml2_type_drivers = $::kickstack::neutron_ml2_type_drivers
+  $ml2_mechanism_drivers = $::kickstack::neutron_ml2_mechanism_drivers
+  $ml2_flat_networks = $::kickstack::neutron_ml2_flat_networks
+  $ml2_vxlan_group = $::kickstack::neutron_ml2_vxlan_group
+  $ml2_vni_ranges = $::kickstack::neutron_ml2_vni_ranges
+  $ml2_enable_security_group = $::kickstack::neutron_ml2_enable_security_group
+  $ml2_firewall_driver = $::kickstack::neutron_ml2_firewall_driver
+  $tenant_network_type = $::kickstack::neutron_tenant_network_type
+
+  case "$::kickstack::neutron_plugin" {
+    'ml2': {
+      if 'gre' in $tenant_network_type {
+        $network_vlan_ranges = ['']
+      } else {
+        $network_vlan_ranges = ["${::kickstack::neutron_physnet}:${::kickstack::neutron_network_vlan_ranges}"]
+      }
+    }
+    default: {
+      $network_vlan_ranges = $tenant_network_type ? {
+      'gre' => '',
+      default => "${::kickstack::neutron_physnet}:${::kickstack::neutron_network_vlan_ranges}",
+      }
+    }
   }
-  $tunnel_id_ranges = $tenant_network_type ? {
-    'gre' => $::kickstack::neutron_tunnel_id_ranges,
-    default => '',
+
+  case "$::kickstack::neutron_plugin" {
+    'ml2': {
+      if 'gre' in $tenant_network_type {
+        $tunnel_id_ranges = ["${::kickstack::neutron_tunnel_id_ranges}"]
+      } else {
+        $tunnel_id_ranges = ['']
+      }
+    }
+    default: {
+      $tunnel_id_ranges = $tenant_network_type ? {
+        'gre' => $::kickstack::neutron_tunnel_id_ranges,
+        default => '',
+      }
+    }
   }
 
   case "$::kickstack::neutron_plugin" {
@@ -22,12 +53,6 @@ class kickstack::neutron::plugin inherits kickstack {
         tunnel_id_ranges => $tunnel_id_ranges,
         package_ensure => $::kickstack::package_version,
       }
-      # This needs to be set for the plugin, not the agent
-      # (the latter is what the Neutron module assumes)
-      neutron_plugin_ovs { 'SECURITYGROUP/firewall_driver':
-        value => 'neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver',
-        require => Class['neutron::plugins::ovs']
-      }
     }
     'linuxbridge': {
       class { "neutron::plugins::linuxbridge":
@@ -35,6 +60,20 @@ class kickstack::neutron::plugin inherits kickstack {
         tenant_network_type => $tenant_network_type,
         network_vlan_ranges => $network_vlan_ranges,
         package_ensure => $::kickstack::package_version,
+      }
+    }
+    'ml2': {
+      class { "neutron::plugins::ml2":
+        type_drivers => $ml2_type_drivers,
+        tenant_network_types => $tenant_network_type,
+        mechanism_drivers => $ml2_mechanism_drivers,
+        flat_networks => $ml2_flat_networks,
+        network_vlan_ranges => $network_vlan_ranges,
+        tunnel_id_ranges => $tunnel_id_ranges,
+        vxlan_group => $ml2_vxlan_group,
+        vni_ranges => $ml2_vni_ranges,
+        enable_security_group => $ml2_enable_security_group,
+        firewall_driver => $ml2_firewall_driver,
       }
     }
   } 
